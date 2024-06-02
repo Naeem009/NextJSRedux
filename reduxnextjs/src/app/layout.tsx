61ec581111cd85ccf447ec0ac274579e6cfef00c
Naeem009 <m.n.shahzad@gmail.com>
2024-06-02 14:09:21 +0500

    some pages updates for fucntion addition

--- reduxnextjs/src/app/layout.tsx
+++ reduxnextjs/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Login from "./(pages)/login/page";
+import Dashboard from "./(pages)/dashboard/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      <Login/>
+      <Dashboard/>
+      </body>
     </html>
   );
 }
